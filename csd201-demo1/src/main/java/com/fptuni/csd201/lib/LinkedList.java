4f053f331ea613520e9063176e919627d491acd9
StevenLongNguyen <91936171+StevenLongNguyen@users.noreply.github.com>
2022-09-20 12:07:25 +0700

    Update LinkedList.java

--- csd201-demo1/src/main/java/com/fptuni/csd201/lib/LinkedList.java
+++ csd201-demo1/src/main/java/com/fptuni/csd201/lib/LinkedList.java
@@ -25,13 +25,25 @@ public class LinkedList {
     
 //    1. `void addToHead(int x)` - add a node with value x  at the head of  a list.
     public void addToHead(int x) {
-        
+        Node p = new Node(x);
+        if (isEmpty()) {
+            head = tail = p;
+        } else {
+            p.next = head;
+            head = p;
+        }
     }
 
 //2. `void addToTail(int x)` - add a node with value x  at the tail of  a list.
     
     public void addToTail(int x) {
-        
+        Node p = new Node(x);
+        if (isEmpty()) {
+            head = tail = p;
+        } else {
+            tail.next = p;
+            tail = p;
+        }
         
     }
     
@@ -39,18 +51,47 @@ public class LinkedList {
 //3. `void addAfter(Node p, int x)` - add a node with value x  after the node p.
 
     public void addAfter(Node p, int x) {
-
+        if(x == -1){
+            addToHead(x); return;
+        }
+        int count = 0;
+        Node p1 = head;
+        while(p1 != null && count < x){
+            p1 = p1.next; count++;
+        }
+        if(p1.next == null && count == x){
+            addToTail(x); return;
+        }
+        p.next = p1.next;
+        p1.next = p; 
         
     }
 
 //4. `void traverse()` - traverse from head to tail and dislay info of all nodes in the list.
     public void traverse() {
-
-        
+        Node p = head;
+        while (p != null) {
+            System.out.print(p.info + "  ");
+            p = p.next;
+        }
+        System.out.println("");
+    
     }
 
 //5. `int deleteFromHead()` - delete the head and return its info.
     public int deleteFromHead() {
+        Node p = head;
+        if (p == null) {
+
+        }
+        //if head in not null and next of head is null, delete the head
+        if (p.next == null) {
+            p = null;
+        } else {
+            head = p.next;
+            head = p.next;
+            //head = head.next
+        }
         return 0;
     }
 //6. `int deleteFromTail()` - delete the tail and return its info.
@@ -65,6 +106,16 @@ public class LinkedList {
 //7. `int deleteAter(Node p)` - delete the node after the node  p  and return its info.
 
     public int deleteAter(Node p) {
+           
+        Node q = p.next;//if p is before tail then remove tail
+        if(q.next == null){
+            tail = p;
+            p.next = null;
+        }
+        else{
+            p.next = q.next;
+            q.next = null;
+        }
         return 0;
     }
 
